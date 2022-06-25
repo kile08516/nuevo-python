f6a13e2f282aeb3d3808ba960478725323421aab
kile08516 <kile08516@gmail.com>
2022-06-25 17:10:32 -0500

    actualizado 15 de junio de 2022

--- principal.py
+++ principal.py
@@ -1,15 +1,22 @@
 
+from Alumno import *
 
-import paquetes_funciones
+persona1=Persona("Nicolas","Guzman"30)
+print("me llamo",persona1.nombre)
+print("mis apellidos",persona1.apellidos)
+print("y tengo",persona1.edad"Años")
+persona1.comer()
+persona1.caminando()
 
-from paquetes_funciones()
+alumno1=Alumno("Miguel","S0202201")
+print(alumno1.promedio())
+print(alumno1.comer())
 
-
-print("Suma:",paquetes_y_funciones.suma(10,25))
-print(paquetes_y_funciones. saludo("Gabriel"))
-
-print("otra suma:" ,suma(30,90))
-
-
+mi_archivo=open("persona.txt","w")
+try
+    mi_archivo.write(f"esta es una persona {alumno1.nombre}n")
+    mi_archivo.write(f"otra {alumno1.nombre}n")
+finally:
+    mi_archivo.close()
 
 
